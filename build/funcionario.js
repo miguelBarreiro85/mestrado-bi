92e3f44af2d779f871e008fe5a8067cddb6a6b25
miguelbarreiro85 <miguelbarreiro85@gmail.com>
2022-05-12 20:25:19 +0100

    add funcionarios

--- build/funcionario.js
+++ build/funcionario.js
@@ -12,11 +12,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const process_1 = require("process");
 const tedious_1 = require("tedious");
 const tedious_2 = require("tedious");
+const tedious_3 = require("tedious");
 const connection_1 = require("./connection");
 const connection = new tedious_1.Connection(connection_1.config);
 function getPessoas() {
     return new Promise((resolve, reject) => {
-        console.log("GET PESSOAS");
         let pessoas = [];
         const request = new tedious_2.Request("select p.id from dbo.pessoa as p inner join dbo.codigo_postal as cp on p.id_codigo_postal = cp.id inner join dbo.concelho as con on con.id = cp.id_concelho inner join dbo.distrito d on d.id = con.id_distrito and d.designacao like 'Leiria' order by p.id offset 0 rows FETCH NEXT 20 ROWS ONLY", (err) => {
             if (err) {
@@ -25,8 +25,6 @@ function getPessoas() {
             resolve(pessoas);
         });
         request.on('row', (columns) => {
-            console.log(columns);
-            let pessoa = {};
             columns.forEach((column) => {
                 if (column.value === null) {
                     console.log('NULL');
@@ -39,24 +37,48 @@ function getPessoas() {
         connection.execSql(request);
     });
 }
+function insertFuncionarios(funcionarios) {
+    return new Promise((resolve, reject) => {
+        console.log("Adicionar funcionarios!!");
+        const bulkLoad = connection.newBulkLoad('dbo.funcionario', function (error, rowCount) {
+            if (error) {
+                console.log("ERRO", error);
+                reject(false);
+            }
+            console.log('inserted %d rows', rowCount);
+            resolve(true);
+        });
+        // setup your columns - always indicate whether the column is nullable
+        bulkLoad.addColumn('id_pessoa', tedious_3.TYPES.Int, { length: 50, nullable: false });
+        bulkLoad.addColumn('preco_hora', tedious_3.TYPES.Float, { nullable: false });
+        bulkLoad.addColumn('ordenado_mensal', tedious_3.TYPES.Float, { nullable: false });
+        // @ts-ignore
+        connection.execBulkLoad(bulkLoad, funcionarios);
+    });
+}
 connection.on('connect', function (err) {
     return __awaiter(this, void 0, void 0, function* () {
         if (err) {
             console.log("ERROR", err);
             return;
         }
-        let pessoas = yield getPessoas();
-        if (!pessoas) {
+        let funcionarios = yield getPessoas();
+        if (!funcionarios) {
             console.log("erro");
             connection.close();
             process_1.exit;
         }
-        pessoas = pessoas.map((pessoa) => {
+        funcionarios = funcionarios.map((pessoa) => {
             pessoa.preco_hora = Math.floor(Math.random() * (50 - 20) + 20);
             pessoa.ordenado_mensal = Math.floor(Math.random() * (3000 - 800) + 800);
             return pessoa;
         });
-        console.log(pessoas);
+        const res = yield insertFuncionarios(funcionarios).catch(err => {
+            console.log("Deu erro ao inserir os funcionarios");
+        });
+        if (res) {
+            console.log("Funcionarios inseridos");
+        }
         connection.close();
     });
 });
--- funcionario.ts
+++ funcionario.ts
@@ -8,7 +8,6 @@ const connection = new Connection(config)
 
 function getPessoas() {
     return new Promise((resolve, reject) => {
-        console.log("GET PESSOAS")
         let pessoas: Object[] = []
         const request = new Request("select p.id from dbo.pessoa as p inner join dbo.codigo_postal as cp on p.id_codigo_postal = cp.id inner join dbo.concelho as con on con.id = cp.id_concelho inner join dbo.distrito d on d.id = con.id_distrito and d.designacao like 'Leiria' order by p.id offset 0 rows FETCH NEXT 20 ROWS ONLY", (err: any) => {
             if (err) {
@@ -17,8 +16,6 @@ function getPessoas() {
             resolve(pessoas);
         });
         request.on('row', (columns) => {
-            console.log(columns)
-            let pessoa : any = {}
             columns.forEach((column) => {
                 if (column.value === null) {
                     console.log('NULL');
@@ -32,23 +29,48 @@ function getPessoas() {
     });
 }
 
+function insertFuncionarios(funcionarios : any) {
+    return new Promise((resolve, reject) => {
+        console.log("Adicionar funcionarios!!")
+        const bulkLoad = connection.newBulkLoad('dbo.funcionario', function (error , rowCount) {
+            if (error) {
+              console.log("ERRO",error)
+              reject(false)
+            }
+            console.log('inserted %d rows', rowCount);
+            resolve(true)
+          });
+          // setup your columns - always indicate whether the column is nullable
+          bulkLoad.addColumn('id_pessoa', TYPES.Int, { length: 50, nullable: false });
+          bulkLoad.addColumn('preco_hora', TYPES.Float, { nullable: false });
+          bulkLoad.addColumn('ordenado_mensal', TYPES.Float, { nullable: false });
+          // @ts-ignore
+          connection.execBulkLoad(bulkLoad, funcionarios);
+    })
+}
+
 connection.on('connect', async function (err) {
     if (err) {
         console.log("ERROR", err);
         return;
     }
-    let pessoas : any = await getPessoas()
-    if(!pessoas) {
+    let funcionarios : any = await getPessoas()
+    if(!funcionarios) {
         console.log("erro")
         connection.close()
         exit
     }
-    pessoas = pessoas.map((pessoa : any) => {
+    funcionarios = funcionarios.map((pessoa : any) => {
         pessoa.preco_hora = Math.floor(Math.random() * (50 - 20) + 20)
         pessoa.ordenado_mensal = Math.floor(Math.random() * (3000 - 800) + 800)
         return pessoa
     })
-    console.log(pessoas)
+    const res : any = await insertFuncionarios(funcionarios).catch(err => {
+        console.log("Deu erro ao inserir os funcionarios")
+    })
+    if(res){
+        console.log("Funcionarios inseridos")
+    }
     connection.close();
 });
 
